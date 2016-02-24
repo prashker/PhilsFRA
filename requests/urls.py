4776c27701f9166465fdcbc5d5097d38f49ee2e3
Samuel Prashker <github@prashker.net>
2016-02-23 23:55:56 -0500

    Can now view requests that have been created on the main page
    Additionally, preliminary code has been done to enable viewing
    each request on a per record basis

--- requests/urls.py
+++ requests/urls.py
@@ -4,4 +4,5 @@ from views import dash
 
 urlpatterns = [
     url(r'^$', dash.all_requests, name='home'),
+    url(r'^view/(?P<id>[0-9]+)/$', dash.view_request, name='view')
 ]
--- requests/views/dash.py
+++ requests/views/dash.py
@@ -1,6 +1,30 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.forms import ModelForm
 
+from requests.models import FeatureRequest
+
+
+class RequestForm(ModelForm):
+    class Meta:
+        model = FeatureRequest
+        fields = ['priority']
 
 def all_requests(request):
-    return render(request, 'dash.html')
+    context = {}
+
+    # There is little logic necessary in the display
+    # We simply want all feature requests.
+    # Todo: Order by priority?
+    context['requests'] = FeatureRequest.objects.all()
+
+    return render(request, 'dash.html', context)
+
+def view_request(request, id):
+
+    featurereq = get_object_or_404(FeatureRequest, pk=id)
+
+    context = {}
+    context['request'] = featurereq
+
+    return render(request, 'view.html', context)
