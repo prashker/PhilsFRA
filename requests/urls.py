41ed7f46b8304f0473ad17b1d97ad196ee20a159
Samuel Prashker <github@prashker.net>
2016-02-24 01:19:42 -0500

    Requests created or updated, priorities are now shifted
    All other requests equal to or greater than that priority
    (by that client) is increased by 1 to retain order

--- requests/urls.py
+++ requests/urls.py
@@ -4,5 +4,6 @@ from views import dash
 
 urlpatterns = [
     url(r'^$', dash.all_requests, name='home'),
-    url(r'^view/(?P<id>[0-9]+)/$', dash.view_request, name='view')
+    url(r'^request/(?P<id>[0-9]+)/$', dash.view_request, name='view'),
+    url(r'^request/new/', dash.create_request, name='create')
 ]
--- requests/views/dash.py
+++ requests/views/dash.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import ModelForm, DateInput
+from django.db.models import F
 
 from requests.models import FeatureRequest
 
@@ -25,13 +26,15 @@ def all_requests(request):
     # There is little logic necessary in the display
     # We simply want all feature requests.
     # Todo: Order by priority?
-    context['requests'] = FeatureRequest.objects.all()
+    context['requests'] = FeatureRequest.objects.all().order_by('priority')
 
     return render(request, 'dash.html', context)
 
 def view_request(request, id):
     featurereq = get_object_or_404(FeatureRequest, pk=id)
     context = dict()
+
+    prev_priority = featurereq.priority
 
     form = None
     updated = False
@@ -40,9 +43,10 @@ def view_request(request, id):
         # Assume we are updating the record
         formvals = RequestForm(request.POST, instance=featurereq)
         if formvals.is_valid():
-            formvals.save()
+            result = formvals.save()
             updated = True
             form = formvals
+            __update_all_other_requests(result)
     else:
         form = RequestForm(instance=featurereq)
 
@@ -51,3 +55,40 @@ def view_request(request, id):
     context['updated'] = updated
 
     return render(request, 'view.html', context)
+
+def create_request(request):
+    context = dict()
+
+    form = None
+    updated = False
+
+    if request.method == "POST":
+        # Assume we are updating the record
+        formvals = RequestForm(request.POST)
+        if formvals.is_valid():
+            result = formvals.save()
+            __update_all_other_requests(result)
+            return redirect('view', id=result.id)
+    else:
+        form = RequestForm()
+
+    context['form'] = form
+
+    return render(request, 'view.html', context)
+
+
+def __update_all_other_requests(featurerequest_obj):
+    # If there's two records with the same priority now,
+    # shift the other and all other records with higher priority up
+    second_record = FeatureRequest.objects.exclude(pk=featurerequest_obj.id).filter(client=featurerequest_obj.client,
+                                                                                    priority=featurerequest_obj.priority)
+    if second_record:
+        FeatureRequest.objects.exclude(
+                pk=featurerequest_obj.id
+            ).filter(
+                client=featurerequest_obj.client,
+                priority__gte=featurerequest_obj.priority
+            ).update(
+                priority=F('priority')+1
+            )
+
