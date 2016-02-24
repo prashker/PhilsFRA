da21fefdf7c815af3eebd155a5d7840b04fbe395
Samuel Prashker <github@prashker.net>
2016-02-24 00:44:47 -0500

    Requests are now displayed on an individual view and can be updated

--- requests/views/dash.py
+++ requests/views/dash.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 
 from requests.models import FeatureRequest
 
@@ -8,10 +8,19 @@ from requests.models import FeatureRequest
 class RequestForm(ModelForm):
     class Meta:
         model = FeatureRequest
-        fields = ['priority']
+        fields = ['title',
+                  'description',
+                  'client',
+                  'priority',
+                  'target_date',
+                  'ticket_url',
+                  'product_area']
+        widgets = {
+            'target_date':  DateInput(attrs={'class': 'datepicker'}),
+        }
 
 def all_requests(request):
-    context = {}
+    context = dict()
 
     # There is little logic necessary in the display
     # We simply want all feature requests.
@@ -21,10 +30,24 @@ def all_requests(request):
     return render(request, 'dash.html', context)
 
 def view_request(request, id):
-
     featurereq = get_object_or_404(FeatureRequest, pk=id)
+    context = dict()
 
-    context = {}
+    form = None
+    updated = False
+
+    if request.method == "POST":
+        # Assume we are updating the record
+        formvals = RequestForm(request.POST, instance=featurereq)
+        if formvals.is_valid():
+            formvals.save()
+            updated = True
+            form = formvals
+    else:
+        form = RequestForm(instance=featurereq)
+
     context['request'] = featurereq
+    context['form'] = form
+    context['updated'] = updated
 
     return render(request, 'view.html', context)
